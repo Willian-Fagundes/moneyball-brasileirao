6923007d5fb1cdb5a9de9c0fbb3bfde4c8eb43c2
Willian Fagundes <61697250+Willian-Fagundes@users.noreply.github.com>
2026-04-05 12:19:20 +0000

    Finished the data extraction and normalization, loaded the data into a sqlite db

---
+++ scripts/tratamento.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sqlite3
+import re
+
+conn = sqlite3.connect('/workspaces/moneyball-brasileirao/databases/brasileirao.db')
+
+df_a_inicio = pd.read_csv('/workspaces/moneyball-brasileirao/data/valores_a_inicio.csv', sep = ',')
+df_b_inicio = pd.read_csv('/workspaces/moneyball-brasileirao/data/valores_b_inicio.csv', sep = ',')
+df_a_fim = pd.read_csv('/workspaces/moneyball-brasileirao/data/valores_c_fim.csv', sep = ',')
+df_b_fim = pd.read_csv('/workspaces/moneyball-brasileirao/data/valores_d_fim.csv', sep = ',')
+df_class = pd.read_csv('/workspaces/moneyball-brasileirao/data/classClubes.csv', sep = ',')
+df_geral = pd.read_csv('/workspaces/moneyball-brasileirao/data/valores_geral.csv', sep = ',')
+
+#Funções auxiliares
+
+def remove_null(df):
+    df = df.iloc[:-1]
+    df = df.drop(df.columns[0], axis=1)
+    df = df.dropna(axis = 1, how = 'all')
+    return df
+
+def rename_colunas_inicio(df):
+    df.columns = ['clube', 'liga', 'valor_01/01/2025', 'numero_jogadores_01/01/2025', 'valor_atual','numero_jogadores_atual', 'diferenca', '%']
+    return df
+
+def rename_colunas_fim(df):
+    df.columns = ['clube', 'liga', 'valor_15/12/2025', 'numero_jogadores_15/12/2025', 'valor_atual','numero_jogadores_atual', 'diferenca', '%']
+    return df
+
+def normalizador_numericos(df):
+    for coluna in df:
+        if df[coluna].dtype == 'str' or df[coluna].dtype == 'object':
+            mask_tem_numero = df[coluna].str.contains(r'\d', na=False)
+            
+            if mask_tem_numero.any():
+                milhar = df[coluna].str.contains(r'k|mil', case=False, na=False)
+                
+                df[coluna] = df[coluna].str.replace(r'\.$', '', regex=True)
+                
+                df[coluna] = df[coluna].str.replace(r'[^\d.-]', '', regex=True)
+                
+                valores_numericos = pd.to_numeric(df[coluna], errors='coerce')
+                
+                df[coluna] = np.where(
+                    milhar,
+                    valores_numericos / 1000, 
+                    valores_numericos
+                )
+            
+    return df
+
+def salvar_db(dfs):
+    conn = sqlite3.connect('/workspaces/moneyball-brasileirao/databases/brasileirao.db')
+    
+    for nome_tabela, df in dfs.items():  
+        df.to_sql(name=nome_tabela, con=conn, if_exists='replace', index=False)    
+    conn.close()
+
+#Ajustes das tabelas antes de carregar no db
+df_a_inicio = remove_null(df_a_inicio)
+df_b_inicio = remove_null(df_b_inicio)
+df_a_fim = remove_null(df_a_fim)
+df_b_fim = remove_null(df_b_fim)
+df_a_inicio = rename_colunas_inicio(df_a_inicio)
+df_b_inicio = rename_colunas_inicio(df_b_inicio)
+df_a_fim = rename_colunas_fim(df_a_fim)
+df_b_fim = rename_colunas_fim(df_b_fim)
+
+df_geral_1 = df_geral.iloc[:-1]
+df_geral_2 = df_geral_1.dropna(axis= 1, how = 'all')
+df_geral_2.columns = ['clube', 'numero_jogadores', 'media_idade', 'estrangeiros', 'media_valor_mercado_', 'valor_mercado_total']
+
+df_class_1 = df_class.dropna(axis= 1, how = 'all')
+df_class_2 = df_class_1.drop(df_class_1.columns[[0, 2]], axis=1)
+df_class_2.columns = ['clube', 'v', 'e', 'd', 'gols', 'sg', 'pontos']
+
+lista_df = [df_a_inicio,df_b_inicio,df_a_fim,df_b_fim, df_geral_2]
+
+lista_df_db = {'df_a_inicio' : df_a_inicio,
+               'df_b_inicio' : df_b_inicio,
+               'df_a_fim' : df_a_fim,
+               'df_b_fim' : df_b_fim,
+               'df_geral' : df_geral_2,
+               'df_class' : df_class_2}
+
+for df in lista_df:
+    normalizador_numericos(df)
+# carregar tabelas em um sql para fazer analises posteriores
+salvar_db(lista_df_db)
+
